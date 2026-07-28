6f806e32f288d3c7bf4eb843dc14e36a965d443f
Prince kumar <pkssjr43@gmail.com>
2026-07-28 09:38:34 +0530

    Time: 7 ms (10.02%), Space: 46.9 MB (33.6%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,16 +11,27 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode,Integer> map = new HashMap<>();
+        // HashMap<ListNode,Integer> map = new HashMap<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //    if(!map.containsKey(temp)){
+        //     map.put(temp,map.getOrDefault(temp,0)+1);
+        //    }
+        //    else{
+        //     return true;
+        //    }
+        //    temp = temp.next;
+        // }
+        // return false;
+
+        HashSet<ListNode>set = new HashSet<>();
         ListNode temp = head;
         while(temp != null){
-           if(!map.containsKey(temp)){
-            map.put(temp,map.getOrDefault(temp,0)+1);
-           }
-           else{
-            return true;
-           }
-           temp = temp.next;
+            if(set.contains(temp)){
+                return true;
+            }
+            set.add(temp);
+            temp = temp.next;
         }
         return false;
 
